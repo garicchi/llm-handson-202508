8edf905573d08e884384565bff209272137b42e3
garicchi <xgaricchi@gmail.com>
2025-08-26 23:09:32 +0900

    Revert "06_manage_memoryサンプルを追加"

--- samples/06_show_prompt.py
+++ samples/06_show_prompt.py
@@ -1,7 +1,6 @@
 #
-# 06_manage_memory.py
-# LLMと会話をするときに、会話履歴を表示する
-# 数字キーを押すと、その記憶を消去できるサンプル
+# 06_show_prompt.py
+# LLMと会話をするときに、プロンプトを表示するサンプル
 #
 
 from typing import List
@@ -40,33 +39,15 @@ system_prompt = system_prompt_template.invoke({})
 # 会話履歴にシステムプロンプトを追加
 chat_histories.extend(system_prompt.to_messages())
 
-# 会話履歴を表示する関数
-def print_histories():
-    print('\n\n### 会話履歴 ###')
-    for i, history in enumerate(chat_histories):
-        print(f'[{i}] {history.type} {history.content}')
-    print('################\n\n')
-
 # 会話ループ
 while True:
-    # 履歴を表示
-    print_histories()
-
     # ユーザー入力を取得
-    user_input = input('入力してください(qキーで終了、数字で記憶消去) > ')
+    user_input = input('入力してください(qキーで終了) > ')
 
     # qキーを押したら終了
     if user_input.lower() == 'q':
         print('終了します')
         break
-
-    # 数字なら、その記憶を消去
-    if user_input.isdigit():
-        selected_number = int(user_input)
-        history = chat_histories[selected_number]
-        print(f'[{selected_number}]の記憶を消します')
-        chat_histories.remove(history)
-        continue
 
     print(f'[ユーザー]: {user_input}')
 
@@ -84,6 +65,12 @@ while True:
     prompt = prompt_template.invoke({
         'user_input_data': user_input
     })
+
+    print(f'''
+#### プロンプト ####
+{prompt.to_string()}
+##################
+''')
 
     # 回答を生成
     assistant_output = llm.invoke(prompt)
